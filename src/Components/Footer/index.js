23d131c6213dca36d3876bb477f8e85b66a5ee02
kshitizshreyansh <kshitizshreyansh@gmail.com>
2024-05-25 00:21:39 +0530

    Initial commit

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './style.css';
+
+import {BsLinkedin, BsGithub, BsYoutube } from "react-icons/bs";
+const FooterComponent = ()=>{
+    
+    return (
+        <>
+            <footer className='myFooter'>
+                <Container>
+                   <Row>
+                        <Col className=''>
+                            <h3 className='head_3'>Follow us :- </h3>
+                            <ul className='socialIconsList'>
+                                <li>
+                                    <a rel="noreferrer" href="http://linkedin.com/in/kshreyansh" target="_blank">
+                                        <BsLinkedin />
+                                    </a>
+                                </li>
+                                
+                                <li>
+                                    <a rel="noreferrer" href="https://github.com/kshitizshreyansh" target="_blank">
+                                        <BsGithub />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a rel="noreferrer" href="https://www.youtube.com/@kshitizShreyansh" target="_blank">
+                                        <BsYoutube />
+                                    </a>
+                                </li> 
+                            </ul>
+                            
+                        </Col>
+                        <Col className='col-12'>
+                            <div className="footerCopyright">
+                                <small>©Developed and Designed by <a href="https://www.youtube.com/@kshitizShreyansh" target="_blank" rel="noreferrer">Kshitiz Shreyansh</a>.</small>
+                            </div>
+                        </Col>
+                   </Row>
+                </Container>
+            </footer>
+        </>
+    )
+}
+
+export default FooterComponent;
---
+++ src/Container/About/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const AboutContainer = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="pd50">
+            <p>
+              Welcome to My Entertainment, your ultimate destination for
+              everything cinema!</p> <br/><p>At My Entertainment, we're passionate about the
+              magic of movies. Whether you're a casual viewer or a die-hard
+              cinephile, we've got you covered with the latest news, reviews,
+              trailers, and everything in between. Our team of dedicated writers
+              and movie buffs scours the cinematic landscape to bring you the
+              most comprehensive coverage of films from Hollywood blockbusters
+              to indie gems and everything in between. We're committed to
+              providing insightful analysis, thoughtful commentary, and engaging
+              content that celebrates the art of filmmaking in all its forms.</p><br/>
+              <p>But My Entertainment is more than just a place to read about
+              movies; it's a community of fellow enthusiasts who share your love
+              for the silver screen. Join the conversation in our forums,
+              connect with like-minded fans, and discover new perspectives on
+              your favorite films. Whether you're looking for recommendations
+              for your next movie night or want to stay up-to-date with the
+              latest industry buzz,My Entertainment is your go-to destination for
+              all things film. So grab your popcorn, settle in, and let's
+              explore the wonderful world of cinema together! Thank you for
+              choosing My Entertainment as your trusted source for movie news,
+              reviews, and more. Lights, camera, action!
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default AboutContainer;
